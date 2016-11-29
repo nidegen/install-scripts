16d7c5f8e20d61c4be5f0b00141b33b275064f37
Nicolas Degen <degen@kapanu.com>
2016-11-29 14:41:51 +0100

    changed data folder structure and fileserver address

--- macOS/macos-setup.sh
+++ macOS/macos-setup.sh
@@ -6,39 +6,10 @@ cp ./bash_profile.txt $HOME/.bash_profile
 
 
 # Homebrew Installs
-brew install bash-completion
-brew install clang-format
-brew install cmake
-brew install git
-brew install go
-brew install imagemagick
-
-brew cask install blender
-brew cask install dropbox
-brew cask install filezilla
-brew cask install flux
-brew cask install github-desktop
-brew cask install google-chrome
-brew cask install google-drive
-brew cask install inkscape
-brew cask install java
-# brew cask install mactex
-brew cask install meshlab
-brew cask install paintbrush
-brew cask install spotify
-brew cask install vlc
-brew cask install xquartz
-
-brew cask install atom
+brew tap Homebrew/bundle
+brew bundle
 apm install sync-settings
 
-
-## Install in App store
-echo "Install in App Store: Twitter"
-echo "Install in App Store: Telegram"
-echo "Install in App Store: OneDrive"
-echo "Install in App Store: Evernote"
-echo "Install in App Store: WOT"
 
 mkdir $HOME/Developer
 mkdir $HOME/Developer/golang
